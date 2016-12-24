0f66b139d7df80e874f97da77c1e360a2c89378c
Scott Low <slow@uvic.ca>
2016-12-23 21:05:12 -0800

    Set up MongoDB connection and skeleton for ordeal query/creation

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var mongoUrl = 'mongodb://localhost:27017/eiao';
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// Listen on all requests
+app.get('*', (req, res) => {
+    if (req.path) {
+        var path = req.path.replace(/\//g, "");
+
+        // Connect to DB
+        MongoClient.connect(mongoUrl, (err, db) => {
+            if (err) throw err
+
+            getOrCreateOrdeal(db, path);
+
+        });
+    }
+});
+
+// Start listening on port 3000. TODO: Change this to 80
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+});
+
+function getOrCreateOrdeal(db, path) {
+    var collection = db.collection('ordeals');
+
+    // Try to find an ordeal by the specified path
+    collection.find({
+        'path': path
+    }).toArray((err, ordeals) => {
+        if (ordeals.length !== 0) {
+            console.log('Found an ordeal');
+        } else {
+            console.log('No ordeals');
+            collection.insert({
+                'path': path
+            }, (err, result) => {
+                console.log('Inserted ordeal ' + path);
+            });
+        }
+    });
+}

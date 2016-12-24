77ae788925d829f727d71fcbdaa808df445a4add
Scott Low <slow@uvic.ca>
2016-12-23 22:31:54 -0800

    Refactored database connection

--- app.js
+++ app.js
@@ -2,22 +2,34 @@ var express = require('express');
 var app = express();
 var mongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var multer = require('multer');
 
+var database = null;
 var mongoUrl = 'mongodb://localhost:27017/eiao';
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-// Listen on all requests
-app.get('*', (req, res) => {
-    if (req.path) {
-        var path = req.path.replace(/\//g, ""); // TODO: Add logic for failing on malformatted URLs
+mongoClient.connect(mongoUrl, (err, db) => {
+    database = db;
+    routes(database);
+});
 
-        // Connect to DB
-        mongoClient.connect(mongoUrl, (err, db) => {
-            if (err) {
-                throw err
-            }
+
+function routes(db) {
+
+    if (!db) {
+        throw "No database";
+    }
+
+    app.post('/ordeal/create/', (req, res) => {
+
+    });
+
+    // Listen on all requests
+    app.get('*', (req, res) => {
+        if (req.path) {
+            var path = req.path.replace(/\//g, ""); // TODO: Add logic for failing on malformatted URLs
 
             var collection = db.collection('ordeals');
             tryGetOrdeal(collection, path).then((ordeal) => {
@@ -25,12 +37,15 @@ app.get('*', (req, res) => {
                     createOrdeal(collection, path);
                 } else {
                     console.log('Found this ordeal: ' + ordeal.path);
-                    res.render('ordeal', { title: 'Ordeal', message: 'Your ordeal: ' + ordeal.path });
+                    res.render('ordeal', {
+                        title: 'Ordeal',
+                        message: 'Your ordeal: ' + ordeal.path
+                    });
                 }
             });
-        });
-    }
-});
+        }
+    });
+}
 
 // Start listening on port 3000. TODO: Change this to 80
 app.listen(3000, () => {

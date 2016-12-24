c16d7c5a2be8a4ee7ca4a3b729d71c3dcdcf335f
Scott Low <slow@uvic.ca>
2016-12-24 00:40:52 -0800

    Basic functionality (image upload + hit counter) complete

--- app.js
+++ app.js
@@ -1,56 +1,100 @@
+// Requirements
 var express = require('express');
 var app = express();
 var mongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var multer = require('multer');
+var path = require('path');
+var winston = require('winston');
 
+// Date connection configuration
 var database = null;
 var mongoUrl = 'mongodb://localhost:27017/eiao';
 
+// Image storage configuration
+var publicDirName = 'public';
+var uploadsDirName = 'uploads';
+var uploadsPath = publicDirName + '/' + uploadsDirName + '/';
+
+var options = multer.diskStorage({
+    destination: uploadsPath,
+    filename: function(req, file, cb) {
+        var ext = path.extname(file.originalname);
+        cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    }
+});
+
+var upload = multer({
+    storage: options
+});
+
+// App configuration
 app.set('views', './views');
 app.set('view engine', 'pug');
+app.use(express.static(publicDirName))
 
+// Logging configuration
+winston.level = 'debug' // TODO: Change to 'log' when publishing
+
+// Connect to MongoDB and globalize the connection
 mongoClient.connect(mongoUrl, (err, db) => {
     database = db;
     routes(database);
 });
 
-
 function routes(db) {
-
     if (!db) {
-        throw "No database";
+        winston.error("Could not connect to database");
     }
 
-    app.post('/ordeal/create/', (req, res) => {
+    // Create a new ordeal whenever /ordeal/create is hit
+    app.post('/ordeal/create', upload.single('image'), (req, res) => {
+        console.log(req.file);
+        var collection = db.collection('ordeals');
+        var path = parsePath(req.body.path);
 
+        var ordeal = {
+            'path': path,
+            'imageName': req.file.filename,
+            'hits': 0
+        }
+
+        collection.insert(ordeal, (err, result) => {
+            res.redirect('/' + path);
+        });
     });
 
-    // Listen on all requests
+    // Listen on all requests and show an ordeal or prompt the creation of one accordingly
     app.get('*', (req, res) => {
         if (req.path) {
-            var path = req.path.replace(/\//g, ""); // TODO: Add logic for failing on malformatted URLs
+            var path = parsePath(req.path);
 
             var collection = db.collection('ordeals');
             tryGetOrdeal(collection, path).then((ordeal) => {
                 if (ordeal == null) {
-                    createOrdeal(collection, path);
+                    // Show create ordeal page
+                    res.render('createOrdeal', {
+                        title: 'Create a new ordeal',
+                        path: path
+                    });
                 } else {
-                    console.log('Found this ordeal: ' + ordeal.path);
+                    // Display ordeal and increment hit counter
                     res.render('ordeal', {
                         title: 'Ordeal',
-                        message: 'Your ordeal: ' + ordeal.path
+                        path: ordeal.path,
+                        image: uploadsDirName + '/' + ordeal.imageName,
+                        hits: ordeal.hits + 1
                     });
+                    incrementOrdealHits(collection, ordeal.path, ordeal.hits);
                 }
             });
         }
     });
 }
 
-// Start listening on port 3000. TODO: Change this to 80
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
-});
+function parsePath(path) {
+    return path.replace(/\//g, ''); // TODO: Add logic for failing on malformatted URLs
+}
 
 function tryGetOrdeal(collection, path) {
     var p = new Promise((resolve, reject) => {
@@ -59,23 +103,34 @@ function tryGetOrdeal(collection, path) {
             'path': path
         }).toArray((err, ordeals) => {
             if (ordeals.length == 0) {
-                console.log('No ordeals');
+                winston.debug('No ordeals found. Resolving null.');
                 resolve(null);
             } else {
-                console.log('Found an ordeal');
+                winston.debug('Found an ordeal. Resolving with data.');
                 resolve(ordeals[0]); // Operating on the assumption that there's only ever one ordeal
             }
         });
     });
 
     return p;
-
 }
 
-function createOrdeal(collection, path) {
-    collection.insert({
+function incrementOrdealHits(collection, path, hits) {
+    // Increment ordeal hits by one
+    var result = collection.update({
         'path': path
-    }, (err, result) => {
-        console.log('Inserted ordeal ' + path);
+    }, {
+        $set: {
+            hits: hits + 1
+        }
     });
+
+    if(!result) {
+        winston.warn("Failed to update hits for " + path);
+    }
 }
+
+// Start listening on port 3000. TODO: Change this to 80
+app.listen(3000, () => {
+    winston.info('We\'re ready to receive your ordeals!');
+});

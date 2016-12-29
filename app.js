fdb5e4025acfa6bdc89658f9ddae6726b1c0049d
Stanley Ayzenberg <sayzenberg@users.noreply.github.com>
2016-12-29 01:14:56 -0800

    Pull code from dev into master (#1)
    * Set up package.json, app.js and .gitignore
    
    * Set up MongoDB connection and skeleton for ordeal query/creation
    
    * Added view engine and simple rendering of data from mongo
    
    * Refactored database connection
    
    * Basic functionality (image upload + hit counter) complete
    
    * Laying foundation for UI
    
    * Added Milligram CSS framework
    
    * Cleaned up UI
    
    * Adding config file to log debug output in Azure
    
    * Adding CSS
    
    * Added leaderboard for top ordeals at /leaderboard
    
    * Added reference to env variable for connection string in Azure
    
    * Switching to listen on port 80
    
    * Attempting to use a port specified by Azure
    
    * Add /manage pane for deleting ordeals
    
    * Refreshed create ordeal UI to avoid disabled input
    
    * Added fallback logic to use local port and db
    
    * Fixed routing issue
    
    * Added form validation for submitting null images
    
    * Added image resizing to keep storage costs low
    
    * Fixed resize logic to remove original images from temporary storage
    
    * Added homepage
    
    * Refactored code to create a REST API
    
    * Changed up wording and made the input field a form
    
    * Fixed the incrementing logic for API-based GET ordeal
    
    * Adding 64 bit node
    
    * Removing node
    
    * Added sharp
    
    * Everything. Is. An. Ordeal.yml
    
    * Remove sharp from repo
    
    * Fixed null reference for hit sum
    
    * Actually removing sharp
    
    * Fixing typo
    
    * Updated homepage counter code
    
    * Replaced aggregate with in-memory hit counter
    
    * Cleaning up comments
    
    * Linked to leaderboard on homepage
    
    * Fixed IE11 bug where form submit failed
    
    * IE11 fixes

---
+++ app.js
@@ -0,0 +1,272 @@
+// Requirements
+var express = require('express');
+var app = express();
+var mongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var multer = require('multer');
+var path = require('path');
+var winston = require('winston');
+var fs = require('fs');
+var sharp = require('sharp');
+var autoReap = require('multer-autoreap');
+
+// Default values for port and db connection string
+var localPort = '3000';
+var localDB = 'mongodb://localhost:27017/eiao';
+
+// Date connection configuration
+var database = null;
+var mongoUrl = process.env.CUSTOMCONNSTR_ordealDB || localDB;
+
+// Image storage configuration
+var publicDirName = 'public';
+var uploadsDirName = 'uploads';
+var uploadsPath = publicDirName + '/' + uploadsDirName + '/';
+var longestImageDimension = 500;
+
+// Total hit count
+var totalHitCounter = 0;
+
+var options = multer.diskStorage({
+    filename: function(req, file, cb) {
+        var ext = path.extname(file.originalname);
+        cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    }
+});
+
+var upload = multer({
+    storage: options
+});
+
+app.use(autoReap);
+
+// App configuration
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static(publicDirName))
+app.use('/milligram', express.static('node_modules/milligram/dist'))
+app.use('/jquery', express.static('node_modules/jquery/dist'))
+
+// Logging configuration
+winston.level = 'debug' // TODO: Change to 'log' when publishing
+
+// Connect to MongoDB and globalize the connection
+mongoClient.connect(mongoUrl, (err, db) => {
+    database = db;
+    calculateTotalHits(database.collection('ordeals'));
+    routes(database);
+});
+
+function routes(db) {
+    if (!db) {
+        winston.error("Could not connect to database");
+    }
+
+    // Create a new ordeal whenever /api/ordeal/create is hit
+    app.post('/api/ordeal/create', upload.single('image'), (req, res) => {
+        if (req.file) {
+            var processedImageName = 'p-' + req.file.filename;
+
+            var fileStream = fs.readFile(req.file.path, (err, data) => {
+                // Resize any image so that its longest side is no longer than longestImageDimension px
+                sharp(data).resize(longestImageDimension, longestImageDimension).min().toFile(uploadsPath + processedImageName, (err) => {
+                    if (err) {
+                        winston.error('Could not save resized image');
+                        winston.error(err);
+                    }
+                    var collection = db.collection('ordeals');
+                    var path = parsePath(req.body.path);
+
+                    var ordeal = {
+                        'path': path,
+                        'imageName': processedImageName,
+                        'hits': 0
+                    }
+
+                    collection.insert(ordeal, (err, result) => {
+                        if (err) {
+                            winston.error('Error creating ordeal ' + path);
+                            winston.error(err);
+                        } else {
+                            winston.info('Created ordeal ' + path);
+                            res.send({
+                                redirect: '/' + path
+                            });
+                        }
+                    });
+                });
+            });
+        } else {
+            // This error is displayed in createOrdeal.pug via jQuery
+            winston.warn('User did not submit an image. Returning 400');
+            res.statusMessage = 'Please upload an image before submitting!';
+            res.status(400).end();
+        }
+    });
+
+    // Delete the data and image file for an ordeal
+    app.delete('/api/ordeal/delete/:path', (req, res) => {
+        var collection = db.collection('ordeals');
+        var path = parsePath(req.params.path);
+
+        // Delete the specified document and also retrieve it to get fileName
+        collection.findAndModify({
+            'path': path
+        }, null, null, {
+            remove: true
+        }, (err, document) => {
+            if (err) {
+                winston.error('Unable to delete ' + path);
+                winston.error(err);
+            } else {
+                // Delete image file corresponding to ordeal
+                fs.unlink(uploadsPath + document.value.imageName, (err) => {
+                    if (err) {
+                        winston.error('Unable to delete image for ' + path);
+                    }
+                });
+            }
+        });
+
+        res.send('Removed ordeal');
+    });
+
+    // Fetch a JSON-formatted object for an ordeal
+    app.get('/api/ordeal/:path', (req, res) => {
+        var collection = db.collection('ordeals');
+
+        var path = parsePath(req.params.path);
+        tryGetOrdeal(collection, path).then((ordeal) => {
+            if (ordeal == null) {
+                res.sendStatus(404);
+            } else {
+                ordeal.hits++;
+                res.send(JSON.stringify(ordeal));
+                incrementOrdealHits(collection, ordeal.path, --ordeal.hits);
+            }
+        });
+    });
+
+    // Display a leaderboard of the top 10 ordeals
+    app.get('/leaderboard', (req, res) => {
+        var collection = db.collection('ordeals');
+
+        collection.find().sort({
+            'hits': -1
+        }).limit(10).toArray((err, topOrdeals) => {
+            res.render('leaderboard', {
+                data: topOrdeals
+            });
+        });
+    });
+
+    // Display a management panel
+    app.get('/manage', (req, res) => {
+        var collection = db.collection('ordeals');
+
+        collection.find().toArray((err, topOrdeals) => {
+            res.render('manage', {
+                data: topOrdeals
+            });
+        });
+    });
+
+    // Listen on all requests and show an ordeal or prompt the creation of one accordingly
+    app.get('/:ordeal', (req, res) => {
+        var ordealPath = req.params.ordeal;
+        if (ordealPath) {
+            var path = parsePath(ordealPath);
+
+            var collection = db.collection('ordeals');
+            tryGetOrdeal(collection, path).then((ordeal) => {
+                if (ordeal == null) {
+                    // Show create ordeal page
+                    winston.debug('Displaying createOrdeal.pug');
+                    res.render('createOrdeal', {
+                        title: 'Create a new ordeal',
+                        path: path
+                    });
+                } else {
+                    // Display ordeal and increment hit counter
+                    winston.debug('Displaying ordeal ' + ordeal.path);
+                    res.render('ordeal', {
+                        title: ordeal.path + ' - Everything is an Ordeal',
+                        path: ordeal.path,
+                        image: uploadsDirName + '/' + ordeal.imageName,
+                        hits: ordeal.hits + 1
+                    });
+                    incrementOrdealHits(collection, ordeal.path, ordeal.hits);
+                }
+            });
+        }
+    });
+
+    // Display the homepage
+    app.get('/', (req, res) => {
+        res.render('homepage', {
+            totalHits: totalHitCounter
+        });
+    });
+}
+
+function parsePath(path) {
+    return path.replace(/\//g, ''); // TODO: Add logic for failing on malformatted URLs
+}
+
+function tryGetOrdeal(collection, path) {
+    var p = new Promise((resolve, reject) => {
+        // Try to find an ordeal by the specified path
+        collection.find({
+            'path': path
+        }).toArray((err, ordeals) => {
+            if (ordeals.length == 0) {
+                resolve(null);
+            } else {
+                var targetOrdeal = ordeals[0];
+                delete targetOrdeal._id;
+                resolve(targetOrdeal); // Operating on the assumption that there's only ever one ordeal
+            }
+        });
+    });
+
+    return p;
+}
+
+function calculateTotalHits(collection)
+{
+    var cursor = collection.find();
+    cursor.each((err, doc) => {
+        if(doc == null)
+        {
+            cursor.toArray((err, items) => {
+                for (var i = 0; i < items.length; i++)
+                {
+                    totalHitCounter += items[i].hits;
+                }
+            });
+        }
+    });
+}
+
+function incrementOrdealHits(collection, path, hits) {
+    // Increment ordeal hits by one
+    var result = collection.update({
+        'path': path
+    }, {
+        $set: {
+            hits: hits + 1
+        }
+    });
+
+    if (!result) {
+        winston.error("Failed to update hits for " + path);
+    }
+
+    // Update the global hit counter
+    totalHitCounter++;
+}
+
+// Start listening on port 80
+app.listen(process.env.PORT || localPort, () => {
+    winston.info('Keeping track of life, one ordeal at a time.');
+});
